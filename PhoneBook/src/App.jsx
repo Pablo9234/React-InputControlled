f67344fc2188bf18e86934882546070e79b50c3f
Pablo Eduardo <pabloedu701@gmail.com>
2024-07-23 13:29:55 -0600

    Working with Json

--- PhoneBook/src/App.jsx
+++ PhoneBook/src/App.jsx
@@ -1,27 +1,10 @@
 import { PhoneBook } from "./PhoneBook";
-export function App() {
 
-  const notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: false,
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: true,
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: false,
-    },
-  ];
+export function App() {
+  
   return (
     <>
-      <PhoneBook
-      notes = {notes}/>
+      <PhoneBook/>
     </>
   );
 }
--- PhoneBook/src/PhoneBook.jsx
+++ PhoneBook/src/PhoneBook.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export function PhoneBook({ notes }) {
-  const [notesState, setNotesState] = useState(notes);
+export function PhoneBook() {
+  const [notesState, setNotesState] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  //Usar useEffect para hacer fetchs
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+    setNotesState(response.data)
+    });
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -17,13 +25,12 @@ export function PhoneBook({ notes }) {
   };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
   const notesToShow = showAll
     ? notesState
-    : notesState.filter(note => note.important === true);
+    : notesState.filter((note) => note.important === true);
 
   return (
     <>
@@ -36,15 +43,16 @@ export function PhoneBook({ notes }) {
           </div>
         </form>
       </div>
-      <h3>{showAll ? 'All' : 'Important'} Notes</h3>
+      <h3>{showAll ? "All" : "Important"} Notes</h3>
       <ul>
         {notesToShow.map((note) => (
           <li key={note.id}>{note.content}</li>
         ))}
       </ul>
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          Show {showAll ? 'important' : 'all'}
+          Show {showAll ? "important" : "all"}
         </button>
       </div>
     </>
