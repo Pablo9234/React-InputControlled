e0289ff4e41346f5ab3a8576355036ed5340c22e
Pablo Eduardo <pabloedu701@gmail.com>
2024-07-24 19:53:23 -0600

    Delete

--- PhoneBook/src/PhoneBook.jsx
+++ PhoneBook/src/PhoneBook.jsx
@@ -35,22 +35,30 @@ export function PhoneBook() {
     ? notesState
     : notesState.filter((note) => note.important === true);
 
-    const toggleImportanceOf = (id) => {
-      const url = `http://localhost:3001/notes/${id}`;
-      const note = notesState.find((n) => n.id === id);
-      const changedNote = { ...note, important: !note.important };
   
-      axios.put(url, changedNote).then((response) => {
+    const handleImportant = (id) => {
+      const url = `http://localhost:3001/notes/${id}`; //Ruta de la nota que quiero cambiar
+      const note = notesState.find((n) => n.id === id); 
+      const changeId = {...note, important: !note.important};
+
+      axios.put(url, changeId).then((response) => {
         setNotesState(
           notesState.map((note) => (note.id !== id ? note : response.data))
         );
       });
     }
-  
+
+    const deletePost = (id) => {
+      axios.delete(`http://localhost:3001/notes/${id}`).then((response) => {
+        setNotesState(notesState.filter((note) => note.id !== id))
+        console.log(notesState)
+      })
+    }
 
   return (
     <>
-      <h1>Notes</h1>
+      <h1>{notesState.length > 0 ? "My notes" : "Create Note"}
+        </h1>
       <div>
         <form onSubmit={addNote}>
           <input value={newNote} onChange={handleNoteChange} />
@@ -64,9 +72,10 @@ export function PhoneBook() {
         {notesToShow.map((note) => (
           <li key={note.id}>
             {note.content}{" "}
-            <button onClick={()=>toggleImportanceOf(note.id)}>
+            <button onClick={()=>handleImportant(note.id)}>
               {note.important ? "Make not important" : "Make important"}
             </button>
+            <button onClick={()=>deletePost(note.id)}>Delete</button>
           </li>
         ))}
       </ul>
